86176500175f31c92b9477830a7b6e5cfc5470d0
Charleston Santos <charleston.santos@gmail.com>
2023-09-06 18:50:07 -0300

    Custom checkout Component

---
+++ force-app/main/default/lwc/b2bPurchaseOrder/b2bPurchaseOrder.js
@@ -0,0 +1,61 @@
+import { LightningElement, api } from 'lwc';
+/**
+ * Pruchase Order Number is a mandatory field at checkout 
+ * Place order should be blocked by this component when 
+ * placed in the payment step before or after the payment component.
+ * 
+ * One page layout: this component may be placed in its own section 
+ * before or after payment.
+ *
+ * Accordion layout: this component may be placed in its own section 
+ * before the payment section.
+ * 
+ * Accordion layout: this component may be placed directly in the 
+ * payment section before or after the payment component.
+ */
+export default class B2bPurchaseOrder extends LightningElement {
+    @api
+    errorMessage;
+    @api
+    placeHolder;
+    purchaseOrderNumber = '';
+
+    /**
+     * Required by checkout to register as a checkout component
+     * 1 = EDIT mode
+     */
+    @api
+    checkoutMode = 1;
+
+    isValidPurchaseOrder(){
+        this.purchaseOrderNumber.trim() != '' && this.purchaseOrderNumber.length > 3
+    }
+
+    /**
+     * Report false and show the error message until the user accepts the Terms
+     */
+    @api
+    get checkValidity() {
+        return this.isValidPurchaseOrder();
+    }
+
+    /**
+     * Report false and show the error message until the pruchase order have being filled
+     * purchaseOrderNumber has reportValidity functionality.
+     * 
+     * One-page Layout: reportValidity is triggered clicking place order.
+     * 
+     * Accordion Layout: reportValidity is triggered clicking each section's 
+     * proceed button.
+     *
+     * @returns boolean
+     */
+    @api
+    reportValidity() {
+        let isValid = ! this.isValidPurchaseOrder();
+        this.showError = isValid;
+ 
+        return isValid;
+    }    
+ 
+}
